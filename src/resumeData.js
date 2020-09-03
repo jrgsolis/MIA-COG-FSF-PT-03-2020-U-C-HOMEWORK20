4262eef0637bb7d2bd803adba2a812987f931d9e
wizeapps <jrgsolis@gmail.com>
2020-09-02 23:11:06 -0400

    Pushing to Heroku React Portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,91 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/jrgsolis",
+    "name": "Jorge Solis",
+    "role": "Solution Architect and Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/jrgsolis",
+    "skypeid": "jrgsolis",
+    "roleDescription": "Excellent communicator, collaborates with C-level executives, business stakeholders, and IT groups to architect enterprise application solutions. Specialist in handling challenging change management projects; known for supporting multiple critical projects simultaneously",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jrgsolis",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/jrgsolis",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"10+ years Software and Solution Architect full skilled with Java Programming and Spring Framework",
+    "address":"Miami",
+    "website":"https://www.linkedin.com/in/jrgsolis",
+    "education":[
+      {
+        "UniversityName":"Itesm",
+        "specialization":"Computer Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2005",
+        "Achievements":"Graduate with Honors"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Visa",
+        "specialization":"Digital Payment Enablement Director",
+        "Achievements":"Enablement Infraestructure in LATAM"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"Spring"
+      },
+      {
+        "skillname":"SQL"
+      },
+      {
+        "skillname":"MERN"
+      },
+      {
+        "skillname":"Microservices"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Dinner Time",
+        "description":"UM BootCamp Project 1",
+        "imgurl":"images/portfolio/dinnertime.png"
+      },
+      {
+        "name":"Eat Da Burger",
+        "description":"UM Bootcamp SQL Node Homework",
+        "imgurl":"images/portfolio/eatdaburger.png"
+      },
+      {
+        "name":"Note Taker",
+        "description":"Node JS Note Taker",  
+        "imgurl":"images/portfolio/notetaker.png"
+      },
+      {
+        "name":"Uber Games",
+        "description":"UM Bootcamp Project 2",
+        "imgurl":"images/portfolio/ubergames.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
